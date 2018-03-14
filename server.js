79a396de43b7f7d4f286f31e2e2503bcef06e1b6
Serra Doll <serra.pres.doll@gmail.com>
2018-03-13 21:26:35 -0400

    clean up

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 //Event Schema
 const Event = require("./models/events");
-// create our instances
 const app = express();
 const router = express.Router();
-//set our port to either a predetermined port number if you have set it up, or 3001
 const port = process.env.API_PORT || 3001;
 
 //db config
@@ -31,8 +29,6 @@ app.use(function(req, res, next) {
     "Access-Control-Allow-Headers",
     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
-
-  //and remove cacheing so we get the most recent comments
   res.setHeader("Cache-Control", "no-cache");
   next();
 });
@@ -42,35 +38,31 @@ router.get("/", function(req, res) {
   res.json({ message: "API Initialized!" });
 });
 
-//adding the /comments route to our /api router
+//adding the /events route to our /api router
 router
   .route("/events")
-  //retrieve all events from the database
   .get(function(req, res) {
     //looks at our Event Schema
     Event.find(function(err, events) {
       if (err) res.send(err);
-      //responds with a json object of our database comments.
       res.json(events);
     });
   })
-  //post new comment to the database
+  //post
   .post(function(req, res) {
     var event = new Event();
-    //body parser lets us use the req.body
     event.title = req.body.title;
     event.description = req.body.description;
-
     event.save(function(err) {
       if (err) res.send(err);
-      res.json({ message: "Event successfully added!" });
+      res.json({ message: "Event added" });
     });
   });
 
 //Use our router configuration when we call /api
 app.use("/api", router);
 
-//starts the server and listens for requests
+//starts the server and listen for req
 app.listen(port, function() {
-  console.log(`api running on port ${port}`);
+  console.log(`api running - ${port}`);
 });
